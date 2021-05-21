b1ed849d074ecde1f80bb2b1a05e37e668ea8b2f
Nehal Ahmed <41865951+nehal2210@users.noreply.github.com>
2021-05-20 23:06:01 -0700

    version 1

---
+++ src/Room.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import "./Room.css"
+import ON from "./assets/on.jpg";
+import OFF from "./assets/off.jpg";
+
+function Room(){
+
+    const [lit,setLit] = useState(true);
+    const [temp,setTemp] = useState(72);
+
+    return(
+        <div className={`room ${lit?'lit':'dark'}`}>
+<div>
+
+    <h1>React Bulb Switch</h1>
+           {lit?<img src={ON} style={{marginTop: 50}}/>:<img src={OFF} style={{marginTop: 50}}/>}
+           <br />
+            <button onClick={()=>{setLit(true)}}>ON</button>
+            <button onClick={()=>{setLit(false)}}>OFF</button>
+            </div>
+
+            <div>
+                <h2>Room Temperature {temp}</h2>
+                <button onClick={()=>{setTemp(temp+1)}}>Increase</button>
+                <button onClick={()=>{setTemp(temp-1)}}>Decrease</button>
+
+            </div>
+        </div>
+    );
+}
+
+export default Room;
